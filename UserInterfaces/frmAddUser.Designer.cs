b6d53d948f90b336d4288d6ccd0afbc49a911833
ilhana-dzeko <75779251+ilhana-dzeko@users.noreply.github.com>
2021-04-17 22:53:03 +0200

    //

--- UserInterfaces/frmAddUser.Designer.cs
+++ UserInterfaces/frmAddUser.Designer.cs
@@ -28,21 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblTelephone = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblFullname = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtFullname = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblEmail.Location = new System.Drawing.Point(82, 364);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 29;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.SystemColors.Window;
+            this.txtEmail.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtEmail.Location = new System.Drawing.Point(82, 380);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(238, 20);
+            this.txtEmail.TabIndex = 28;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnDelete.Location = new System.Drawing.Point(206, 326);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(111, 23);
+            this.btnDelete.TabIndex = 27;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnEdit.Location = new System.Drawing.Point(82, 326);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(118, 23);
+            this.btnEdit.TabIndex = 26;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnAdd.Location = new System.Drawing.Point(82, 286);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(235, 34);
+            this.btnAdd.TabIndex = 25;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            // 
+            // lblTelephone
+            // 
+            this.lblTelephone.AutoSize = true;
+            this.lblTelephone.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblTelephone.Location = new System.Drawing.Point(79, 232);
+            this.lblTelephone.Name = "lblTelephone";
+            this.lblTelephone.Size = new System.Drawing.Size(41, 13);
+            this.lblTelephone.TabIndex = 24;
+            this.lblTelephone.Text = "Phone:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblPassword.Location = new System.Drawing.Point(79, 187);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 23;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblFullname
+            // 
+            this.lblFullname.AutoSize = true;
+            this.lblFullname.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblFullname.Location = new System.Drawing.Point(79, 138);
+            this.lblFullname.Name = "lblFullname";
+            this.lblFullname.Size = new System.Drawing.Size(57, 13);
+            this.lblFullname.TabIndex = 22;
+            this.lblFullname.Text = "Full Name:";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblUsername.Location = new System.Drawing.Point(79, 95);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 21;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.BackColor = System.Drawing.SystemColors.Window;
+            this.txtTelephone.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtTelephone.Location = new System.Drawing.Point(79, 248);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(238, 20);
+            this.txtTelephone.TabIndex = 20;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.Window;
+            this.txtPassword.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtPassword.Location = new System.Drawing.Point(79, 203);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(238, 20);
+            this.txtPassword.TabIndex = 19;
+            // 
+            // txtFullname
+            // 
+            this.txtFullname.BackColor = System.Drawing.SystemColors.Window;
+            this.txtFullname.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtFullname.Location = new System.Drawing.Point(79, 154);
+            this.txtFullname.Name = "txtFullname";
+            this.txtFullname.Size = new System.Drawing.Size(238, 20);
+            this.txtFullname.TabIndex = 18;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.BackColor = System.Drawing.SystemColors.Window;
+            this.txtUsername.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.txtUsername.Location = new System.Drawing.Point(79, 111);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(238, 20);
+            this.txtUsername.TabIndex = 17;
             // 
             // frmAddUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1039, 637);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTelephone);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblFullname);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtTelephone);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtFullname);
+            this.Controls.Add(this.txtUsername);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmAddUser";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmAddUser";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblTelephone;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblFullname;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtFullname;
+        private System.Windows.Forms.TextBox txtUsername;
     }
 }
